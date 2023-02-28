7c024d2fb2c1fc0591b3b0aa2c29472860ffadee
Jeremie Bresson <jeremie.bresson@unblu.com>
2023-02-28 10:54:09 +0100

    Add "Changes since last review on MR"

---
+++ mr-changes-since-last-review/AddReviewAtComment.java
@@ -0,0 +1,114 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS info.picocli:picocli:4.6.3
+//DEPS org.gitlab4j:gitlab4j-api:5.0.1
+//JAVA 17
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.models.MergeRequest;
+import org.gitlab4j.api.models.User;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "AddReviewAtComment", mixinStandardHelpOptions = true, version = "AddReviewAtComment 0.1", description = "Creates a comment in a GitLab MR")
+class AddReviewAtComment implements Callable<Integer> {
+
+    private static final String CONFIG_FILE_INITIAL_CONTENT = """
+            GITLAB_URL=https://gitlab.com
+            GITLAB_AUTH_VALUE=
+            """;
+
+    @Parameters(index = "0", description = "The project id or project path")
+    private String project;
+
+    @Parameters(index = "1", description = "The MR id")
+    private Long mrId;
+
+    @Option(names = { "-c", "--config" }, description = "configuration file location")
+    String configFile;
+
+    @Override
+    public Integer call() throws Exception {
+        Path file;
+        if (configFile != null) {
+            file = Paths.get(configFile);
+        } else {
+            file = configFile(Paths.get(""));
+        }
+        System.out.println("Reading config: " + file.toAbsolutePath());
+        Properties p = configProperties(file);
+        String gitLabUrl = readProperty(p, "GITLAB_URL", "https://gitlab.com");
+        String gitLabAuthValue = readProperty(p, "GITLAB_AUTH_VALUE");
+
+        try (GitLabApi gitLabApi = new GitLabApi(gitLabUrl, gitLabAuthValue)) {
+            User user = gitLabApi.getUserApi()
+                    .getCurrentUser();
+
+            MergeRequest mr = gitLabApi.getMergeRequestApi()
+                    .getMergeRequest(project, mrId);
+
+            String body = "[" + user.getName() + "](" + user.getWebUrl() + ") reviewed the MR ([changes](" + mr.getWebUrl() + "/diffs?start_sha=" + mr.getSha() + ") since then)";
+            gitLabApi.getNotesApi()
+                    .createMergeRequestNote(project, mrId, body);
+
+            System.out.println("Comment added on " + mr.getWebUrl());
+        }
+        return 0;
+    }
+
+    public static Properties configProperties(Path configFile) {
+        try (InputStream is = new FileInputStream(configFile.toFile())) {
+            Properties properties = new Properties();
+            properties.load(is);
+            return properties;
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not read config file", e);
+        }
+    }
+
+    public static Path configFile(Path root) {
+        Path configFile = root.toAbsolutePath()
+                .resolve("gitlab-config.properties");
+        if (!Files.isRegularFile(configFile)) {
+            try {
+                Files.writeString(configFile, CONFIG_FILE_INITIAL_CONTENT);
+                throw new IllegalStateException(String.format("Configuration file '%s' does not exist. An empty configuration file was created", configFile.toAbsolutePath()));
+            } catch (IOException e) {
+                throw new IllegalStateException("Can not write initial config file", e);
+            }
+        }
+        return configFile;
+    }
+
+    public static String readProperty(Properties p, String key) {
+        if (!p.containsKey(key)) {
+            throw new IllegalStateException(String.format("Configuration file does not contains key '%s'", key));
+        }
+        String value = p.getProperty(key);
+        if (value == null || value.isBlank()) {
+            throw new IllegalStateException(String.format("Key '%s' is not defined in configuration file", key));
+        }
+        return value;
+    }
+
+    public static String readProperty(Properties p, String key, String defaultValue) {
+        return p.getProperty(key, defaultValue);
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new AddReviewAtComment()).execute(args);
+        System.exit(exitCode);
+    }
+}
