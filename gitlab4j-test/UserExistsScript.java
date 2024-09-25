98aaa54f1e3375e632afeba169c2e23c1c27b480
Jeremie Bresson <jeremie.bresson@unblu.com>
2024-09-25 10:27:40 +0200

    Add UserExistsScript
    Test for https://github.com/gitlab4j/gitlab4j-api/pull/1157

---
+++ gitlab4j-test/UserExistsScript.java
@@ -0,0 +1,104 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS info.picocli:picocli:4.6.3
+//DEPS https://github.com/strangelookingnerd/gitlab4j-api/commit/e5cfb2bfa9a49db734ca0a33bddeb678f09b42c4
+//JAVA 17
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.models.EpicIssue;
+import org.gitlab4j.api.models.Issue;
+import org.gitlab4j.api.models.IterationFilter;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "UserExistsScript", mixinStandardHelpOptions = true, version = "UserExistsScript 0.1", description = "Tests for GitLab4J")
+public class UserExistsScript implements Callable<Integer> {
+
+    private static final String CONFIG_FILE_INITIAL_CONTENT = """
+            GITLAB_URL=https://gitlab.com
+            GITLAB_AUTH_VALUE=
+            """;
+
+    @Option(names = { "-u", "--username" }, description = "username", required = true)
+    private String username;
+
+    @Option(names = { "-c", "--config" }, description = "configuration file location")
+    String configFile;
+
+    @Override
+    public Integer call() throws Exception {
+        Path file;
+        if (configFile != null) {
+            file = Paths.get(configFile);
+        } else {
+            file = configFile(Paths.get(""));
+        }
+        System.out.println("Reading config: " + file.toAbsolutePath());
+        final Properties prop = configProperties(file);
+        final String gitLabUrl = readProperty(prop, "GITLAB_URL", "https://gitlab.com");
+        final String gitLabAuthValue = readProperty(prop, "GITLAB_AUTH_VALUE");
+
+        try (GitLabApi gitLabApi = new GitLabApi(gitLabUrl, gitLabAuthValue)) {
+            var result = gitLabApi.getUserApi().exists(username);
+            System.out.println(result);
+        }
+        return 0;
+    }
+
+    public static Properties configProperties(final Path configFile) {
+        try (InputStream is = new FileInputStream(configFile.toFile())) {
+            final Properties properties = new Properties();
+            properties.load(is);
+            return properties;
+        } catch (final IOException e) {
+            throw new IllegalStateException("Can not read config file", e);
+        }
+    }
+
+    public static Path configFile(final Path root) {
+        final Path configFile = root.toAbsolutePath()
+                .resolve("gitlab-config.properties");
+        if (!Files.isRegularFile(configFile)) {
+            try {
+                Files.writeString(configFile, CONFIG_FILE_INITIAL_CONTENT);
+                throw new IllegalStateException(String.format("Configuration file '%s' does not exist. An empty configuration file was created", configFile.toAbsolutePath()));
+            } catch (final IOException e) {
+                throw new IllegalStateException("Can not write initial config file", e);
+            }
+        }
+        return configFile;
+    }
+
+    public static String readProperty(final Properties p, final String key) {
+        if (!p.containsKey(key)) {
+            throw new IllegalStateException(String.format("Configuration file does not contains key '%s'", key));
+        }
+        final String value = p.getProperty(key);
+        if (value == null || value.isBlank()) {
+            throw new IllegalStateException(String.format("Key '%s' is not defined in configuration file", key));
+        }
+        return value;
+    }
+
+    public static String readProperty(final Properties p, final String key, final String defaultValue) {
+        return p.getProperty(key, defaultValue);
+    }
+
+    public static void main(final String... args) {
+        final int exitCode = new CommandLine(new UserExistsScript()).execute(args);
+        System.exit(exitCode);
+    }
+}
