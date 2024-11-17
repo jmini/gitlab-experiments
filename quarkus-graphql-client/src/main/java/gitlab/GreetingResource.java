9b2fcec5ecf0fdd944b2a783213fbc390cce0d3a
Jeremie Bresson <jeremie.bresson@unblu.com>
2024-11-17 09:26:47 +0100

    quarkus graphql client Fixes

--- quarkus-graphql-client/src/main/java/gitlab/GreetingResource.java
+++ quarkus-graphql-client/src/main/java/gitlab/GreetingResource.java
@@ -3,6 +3,9 @@ package gitlab;
 import java.util.List;
 
 import gitlab.api.WorkitemClientApi;
+import gitlab.model.Group;
+import gitlab.model.NotesFilterType;
+import gitlab.model.Project;
 import gitlab.model.WorkItemConnection;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -25,13 +28,32 @@ public class GreetingResource {
     }
 
     @GET
+    @Path("/group")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String readGroup() {
+        Group response = api.group("tech-marketing/demos/gitlab-agile-demo/initech", true, null);
+        return response.toString();
+    }
+
+    @GET
+    @Path("/project")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String readProject() {
+        Project response = api.project("tech-marketing/demos/gitlab-agile-demo/initech/music-store/parent-portal", true, null);
+        return response.toString();
+    }
+
+    @GET
     @Path("/hardcoded")
     @Produces(MediaType.TEXT_PLAIN)
     public String read() {
-        String p = "stage/main/product-management";
+        String p = "tech-marketing/demos/gitlab-agile-demo/initech";
         List<String> r = List.of(
-                "&1");
-        WorkItemConnection response = api.workItemsByReference(p, r);
+                "tech-marketing/demos/gitlab-agile-demo/initech/music-store&2",
+                "tech-marketing/demos/gitlab-agile-demo/initech&2",
+                "tech-marketing/demos/gitlab-agile-demo/initech&5",
+                "tech-marketing/demos/gitlab-agile-demo/initech/music-store/parent-portal#2");
+        WorkItemConnection response = api.workItemsByReference(p, r, NotesFilterType.ONLY_COMMENTS);
         return response.toString();
     }
 
@@ -39,7 +61,7 @@ public class GreetingResource {
     @Path("/read")
     @Produces(MediaType.TEXT_PLAIN)
     public String read(@QueryParam("path") String path, @QueryParam("refs") List<String> refs) {
-        WorkItemConnection response = api.workItemsByReference(path, refs);
+        WorkItemConnection response = api.workItemsByReference(path, refs, NotesFilterType.ONLY_COMMENTS);
         return response.toString();
     }
 }
