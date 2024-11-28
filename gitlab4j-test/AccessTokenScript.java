d7cf6d7493e76a5611a219424fb984335e8104ac
Jeremie Bresson <jeremie.bresson@unblu.com>
2024-11-28 13:49:28 +0100

    Add AccessTokenScript
    Test for https://github.com/gitlab4j/gitlab4j-api/pull/1191

---
+++ gitlab4j-test/AccessTokenScript.java
@@ -0,0 +1,143 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS info.picocli:picocli:4.6.3
+//DEPS https://github.com/gitlab4j/gitlab4j-api/commit/5805b41bae64b6c287abdbd51f63ce84d1ec8b90
+//JAVA 17
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+
+import org.gitlab4j.api.Constants.ProjectAccessTokenScope;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.AccessLevel;
+import org.gitlab4j.api.models.ProjectAccessToken;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "AccessTokenScript", mixinStandardHelpOptions = true, version = "AccessTokenScript 0.1", description = "Tests for GitLab4J")
+public class AccessTokenScript implements Callable<Integer> {
+
+    private static final String CONFIG_FILE_INITIAL_CONTENT = """
+            GITLAB_URL=https://gitlab.com
+            GITLAB_AUTH_VALUE=
+            """;
+
+    @Parameters(index = "0", description = "action to execute", defaultValue = "GET")
+    private Action action;
+
+    @Option(names = { "-c", "--config" }, description = "configuration file location")
+    String configFile;
+
+    @Option(names = { "-v", "--verbose" }, description = "log http trafic")
+    Boolean logHttp;
+
+    private static enum Action {
+        GET
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        Path file;
+        if (configFile != null) {
+            file = Paths.get(configFile);
+        } else {
+            file = configFile(Paths.get(""));
+        }
+        System.out.println("Reading config: " + file.toAbsolutePath());
+        final Properties prop = configProperties(file);
+        final String gitLabUrl = readProperty(prop, "GITLAB_URL", "https://gitlab.com");
+        final String gitLabAuthValue = readProperty(prop, "GITLAB_AUTH_VALUE");
+
+        try (GitLabApi gitLabApi = createGitLabApi(gitLabUrl, gitLabAuthValue)) {
+            switch (action) {
+            case GET:
+                var selfPersonalAccessToken = gitLabApi.getPersonalAccessTokenApi()
+                        .getPersonalAccessToken();
+                System.out.println(selfPersonalAccessToken);
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + action);
+            }
+        }
+        return 0;
+    }
+
+    private GitLabApi createGitLabApi(String gitLabUrl, String gitLabAuthValue) {
+        if (logHttp != null && logHttp) {
+            return new GitLabApi(gitLabUrl, gitLabAuthValue)
+                .withRequestResponseLogging(java.util.logging.Level.INFO) ;
+        }
+        return new GitLabApi(gitLabUrl, gitLabAuthValue);
+    }
+
+    private void ensureExists(Object value, String optionName) {
+        if (value == null) {
+            throw new IllegalStateException("--" + optionName + " must be set");
+        }
+    }
+
+    private Object idOrPath(String value) {
+        if (value.matches("[0-9]+")) {
+            return Long.valueOf(value);
+        }
+        return value;
+    }
+
+    public static Properties configProperties(final Path configFile) {
+        try (InputStream is = new FileInputStream(configFile.toFile())) {
+            final Properties properties = new Properties();
+            properties.load(is);
+            return properties;
+        } catch (final IOException e) {
+            throw new IllegalStateException("Can not read config file", e);
+        }
+    }
+
+    public static Path configFile(final Path root) {
+        final Path configFile = root.toAbsolutePath()
+                .resolve("gitlab-config.properties");
+        if (!Files.isRegularFile(configFile)) {
+            try {
+                Files.writeString(configFile, CONFIG_FILE_INITIAL_CONTENT);
+                throw new IllegalStateException(String.format("Configuration file '%s' does not exist. An empty configuration file was created", configFile.toAbsolutePath()));
+            } catch (final IOException e) {
+                throw new IllegalStateException("Can not write initial config file", e);
+            }
+        }
+        return configFile;
+    }
+
+    public static String readProperty(final Properties p, final String key) {
+        if (!p.containsKey(key)) {
+            throw new IllegalStateException(String.format("Configuration file does not contains key '%s'", key));
+        }
+        final String value = p.getProperty(key);
+        if (value == null || value.isBlank()) {
+            throw new IllegalStateException(String.format("Key '%s' is not defined in configuration file", key));
+        }
+        return value;
+    }
+
+    public static String readProperty(final Properties p, final String key, final String defaultValue) {
+        return p.getProperty(key, defaultValue);
+    }
+
+    public static void main(final String... args) {
+        final int exitCode = new CommandLine(new AccessTokenScript()).execute(args);
+        System.exit(exitCode);
+    }
+}
